29d0a72ac03812caceb7c9b8b408330030779d5f
jasminehilton <jasminehilton1@live.com>
2023-07-20 17:27:07 -0700

    added functions to add tweets to the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,81 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = function(tweetData) {
+  const $tweet = $(`<article class="tweet" >
+  <header class="tweet-header" >
+    <div class="tweet-header-div">
+      <div class="tweet-header-left">
+        <img src= ${tweetData.user.avatars}>
+        ${tweetData.user.name}
+      </div>
+      <div class="tweet-header-right">
+        ${tweetData.user.handle}
+      </div>
+    </div>
+  </header>
+  <div class="tweet-material">
+    ${tweetData.content.text}
+  </div>
+  <footer>
+    <hr>
+    <div class="tweet-footer-div">
+      <div id="X DAYS AGO" >
+        ${tweetData.created_at}
+      </div>
+      <div>
+        <i class="fa-solid fa-flag tweet-emojis" ></i>
+        <i class="fa-solid fa-retweet tweet-emojis"></i>
+        <i class="fa-solid fa-heart tweet-emojis"></i>
+      </div>
+    </div>
+  </footer>
+</article>`);
+  return $tweet;
+};
+
+const renderTweets = function(tweets) {
+
+  for (let tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('#tweets-container').append($tweet);
+    
+  }
+ 
+};
+
+
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+
+// wait for page to be ready
+$(function() {
+  // loading the tweets
+  renderTweets(data);
+ 
+});
